7af5a8a73a892dc3059918ce3b198c7d2489707f
OArikui <Arikui57@outlook.jp>
2026-06-07 11:08:42 +0900

    camstatをついか

--- main/edit_database.py
+++ main/edit_database.py
@@ -4,10 +4,10 @@ import cv2
 import numpy as np
 import pandas as pd
 import sqlite3
-from picture_analysis import get_cam_status ,min2 ,get_west_direction ,get_size
+from picture_analysis import get_cam_status ,min2 ,get_west_direction ,seeing
 
-def get_pic_info(pic_path):
-    cam_status = get_cam_status(pic_path)#これは辞書型
+def get_pic_info(pic_path, camera_contents):
+    cam_status = get_cam_status(pic_path.replace(pic_path.split(".")[-1], "txt"), contents=camera_contents)
     min2_status = min2(pic_path)
     west_direction = get_west_direction(pic_path)
     return list(cam_status.values()) + list(min2_status.values()) + [west_direction]
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         databases={c:[] for c in columns}
         for pic_path in pic_pathes:
             pic=cv2.imread(pic_path,cv2.IMREAD_UNCHANGED)
-            pic_info = get_pic_info(pic_path)
+            pic_info = get_pic_info(pic_path, setting["camera_columns"].split(","))
             for i, column in enumerate(columns):
                 databases[column].append(pic_info[i])
         df = pd.DataFrame(databases)
--- main/picture_analysis.py
+++ main/picture_analysis.py
@@ -1,16 +1,20 @@
 import glob
 import os
 
-def get_cam_status(pic_path):
+def get_cam_status(text,contents):
     # ここにカメラの状態を取得するコードを実装してください
-    return {"cam_status": "example_status"}
+    if type(text) == str:
+        txt = {line.split("=")[0]: line.split("=")[1] for line in open(text, "r").read().split("\n") if "=" in line}
+    elif type(text) == 'TextIOWrapper':
+        txt = {line.split("=")[0]: line.split("=")[1] for line in text.read().split("\n") if "=" in line}
+    re={}
+    for c in contents:
+        re[c] = txt.get(c, "cant_find")  # カラムに対応する値を取得、存在しない場合は"N/A"を返す
+    return re
 
-def min2(pic):
-    options = settings["min2_options"].split(",")
+def min2(pic):#ver1.0 https://github.com/fujitenmon-coder/seeing/releases/tag/MIN2_ver1.0
+
+def seeing():
 
 def get_west_direction(pic):#多重露光による事後解析系太陽東西取得を採択する場合
     pass
-
-def get_size(pic):
-    #太陽の画像上のサイズ
-
